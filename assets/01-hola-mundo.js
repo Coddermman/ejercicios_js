0c3589dc8dc0b74276d88f88621b43d29226598d
Victor Gómez <gomezcastillovictor84@gmail.com>
2022-09-14 18:03:35 -0300

    primer javascript

---
+++ assets/01-hola-mundo.js
@@ -0,0 +1,10 @@
+let nombre=prompt("ingrese su nombre")
+console.log(nombre)
+let apellido=prompt("Ingrese su apellido");
+console.log(apellido);
+const E= " "
+let Edad=prompt("ingrese tu año de nacimiento");
+console.log(Edad);
+alert("hola"+ E + nombre+E+apellido);
+let anos=2022-Edad
+alert("Tienes"+E+anos+ E+"años")
